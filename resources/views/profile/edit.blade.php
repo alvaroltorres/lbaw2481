2b5e71ade9249e4ddcd2e1929742941f6872ed2c
Álvaro Luís Dias Amaral Alvim Torres <up202208954@up.pt>
2024-12-09 17:49:18 +0000

    Added CSS styling for Profile and Edit Profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="profile-div">
         <h1>{{ __('Edit Profile') }}</h1>
 
         @if (session('status') === 'profile-updated')
@@ -15,72 +15,77 @@
         </div>
 
         <!-- Profile Picture Upload Form -->
-        <form action="{{ route('profile.picture.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('profile.picture.store') }}" method="POST" enctype="multipart/form-data" class="card mb-4">
             @csrf
-            <div class="form-group">
-                <label for="profile_picture">{{ __('Upload Profile Picture:') }}</label>
-                <input type="file" name="profile_picture" id="profile_picture" accept="image/*" required>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="profile_picture">{{ __('Upload Profile Picture:') }}</label>
+                    <input type="file" name="profile_picture" id="profile_picture" accept="image/*" required>
+                </div>
+                <button type="submit" class="btn btn-primary">{{ __('Upload') }}</button>
             </div>
-            <button type="submit" class="btn btn-primary">{{ __('Upload') }}</button>
+        </form>
 
-        <form method="POST" action="{{ route('profile.update') }}">
+        <form method="POST" action="{{ route('profile.update') }}" class="card">
             @csrf
             @method('PATCH')
 
-            <!-- Username -->
-            <div class="form-group">
-                <label for="username">{{ __('Username') }}</label>
-                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username', $user->username) }}" required autofocus placeholder="Enter your username">
+            <div class="card-body">
+                <!-- Username -->
+                <div class="form-group">
+                    <label for="username">{{ __('Username') }}</label>
+                    <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username', $user->username) }}" required autofocus placeholder="Enter your username">
 
-                @error('username')
-                <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+                    @error('username')
+                    <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
-            <!-- Full Name -->
-            <div class="form-group">
-                <label for="fullname">{{ __('Full Name') }}</label>
-                <input id="fullname" type="text" class="form-control @error('fullname') is-invalid @enderror" name="fullname" value="{{ old('fullname', $user->fullname) }}" required placeholder="Enter your full name">
+                <!-- Full Name -->
+                <div class="form-group">
+                    <label for="fullname">{{ __('Full Name') }}</label>
+                    <input id="fullname" type="text" class="form-control @error('fullname') is-invalid @enderror" name="fullname" value="{{ old('fullname', $user->fullname) }}" required placeholder="Enter your full name">
 
-                @error('fullname')
-                <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+                    @error('fullname')
+                    <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
-            <!-- Email -->
-            <div class="form-group">
-                <label for="email">{{ __('Email') }}</label>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', $user->email) }}" required placeholder="Enter your email address">
+                <!-- Email -->
+                <div class="form-group">
+                    <label for="email">{{ __('Email') }}</label>
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', $user->email) }}" required placeholder="Enter your email address">
 
-                @error('email')
-                <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
-            <!-- Tax ID -->
-            <div class="form-group">
-                <label for="nif">{{ __('Tax ID') }}</label>
-                <input id="nif" type="text" class="form-control @error('nif') is-invalid @enderror" name="nif" value="{{ old('nif', $user->nif) }}" required placeholder="Enter your tax ID">
+                <!-- Tax ID -->
+                <div class="form-group">
+                    <label for="nif">{{ __('Tax ID') }}</label>
+                    <input id="nif" type="text" class="form-control @error('nif') is-invalid @enderror" name="nif" value="{{ old('nif', $user->nif) }}" required placeholder="Enter your tax ID">
 
-                @error('nif')
-                <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
+                    @error('nif')
+                    <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
-            <!-- Other Fields (if necessary) -->
+                <!-- Other Fields (if necessary) -->
 
-            <div class="form-group mt-3">
-                <button type="submit" class="btn btn-primary">
-                    {{ __('Save Changes') }}
-                </button>
+                <div class="form-group mt-3">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Save Changes') }}
+                    </button>
+                </div>
             </div>
         </form>
     </div>
