6f7e25a18d298fe7be518adfd43b5ef23e7e11dc
tomy <up202208415@up.pt>
2024-12-22 18:16:35 +0000

    last fix

--- database/alters.sql
+++ database/alters.sql
@@ -18,7 +18,11 @@ CREATE TABLE follow_auctions (
 ALTER TABLE "User"
     ADD COLUMN credits NUMERIC(8, 2) DEFAULT 0.00;
 
-
+CREATE TABLE password_reset_tokens (
+                                       email VARCHAR(255) PRIMARY KEY,
+                                       token VARCHAR(255) NOT NULL,
+                                       created_at TIMESTAMP NULL
+);
 
 -- Add foreign key constraint to 'winner_id' referencing the 'id' column in the 'users' table
 ALTER TABLE "Auction"
